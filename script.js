9ac9c657612589d87a8a5a1fbc1321e92953dd71
Kashish Goyal <kashish1782004@gmail.com>
2024-09-29 22:01:49 +0530

    first page of my portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".typing-text",{
+    strings:["Frontend Developer","Freelancer","Full Stack Developer","Programmer"],
+    typeSpeed:160,
+    backSpeed:160,
+    loop:true,
+    loopCount: Infinity
+})
