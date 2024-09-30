27393407389c84f1b8a38eb7e259729b17f1c7aa
Kashish Goyal <kashish1782004@gmail.com>
2024-09-30 21:40:37 +0530

    about me section in process

--- script.js
+++ script.js
@@ -14,3 +14,26 @@ Shery.imageEffect("#left-div img", {
     // debug: true,
     gooey: true,
   });
+
+
+  function opentab(tabName) {
+    // Hide all tab contents
+    var tabContents = document.getElementsByClassName("tab-contents");
+    for (var i = 0; i < tabContents.length; i++) {
+        tabContents[i].classList.remove("active-tab");
+    }
+
+    // Show the selected tab content
+    var selectedTab = document.getElementById(tabName);
+    selectedTab.classList.add("active-tab");
+
+    // Remove active class from all tab links
+    var tabLinks = document.getElementsByClassName("tab-links");
+    for (var i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].classList.remove("active-link");
+    }
+
+    // Add active class to the selected tab link
+    var selectedLink = document.querySelector(`.tab-links[onclick="opentab('${tabName}')"]`);
+    selectedLink.classList.add("active-link");
+}
